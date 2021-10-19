816f2e44326c26427d9fd3ccd1ce3e5ef9bb18e5
Martin Burolla <mburolla@xpanxion.com>
2021-10-19 13:53:17 -0400

    update

--- src/main/java/JavaOOP/Main.java
+++ src/main/java/JavaOOP/Main.java
@@ -2,8 +2,16 @@ package JavaOOP;
 
 public class Main {
     public static void main(String[] args) {
+        Person p = new Person("08/29/1970", "Marty");
+        System.out.println(p.speak());
+        System.out.println(p.getYearsOld());
 
-        var d = new Dog("today", "Scooby");
+        Cat c = new Cat("02/02/2018", "Gypsy");
+        System.out.println(c.speak());
+        System.out.println(c.getYearsOld());
+
+        Dog d = new Dog("10/10/2020", "Booch");
         System.out.println(d.speak());
+        System.out.println(d.getYearsOld());
     }
 }
